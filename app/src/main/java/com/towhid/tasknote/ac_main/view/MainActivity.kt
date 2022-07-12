50535dc3647d8fc629366cd6a4b380beadfdb2c2
trsimanto <simanto14feb@gmail.com>
2022-07-12 10:17:41 +0600

    package formatting

--- app/src/main/java/com/towhid/tasknote/ac_main/view/MainActivity.kt
+++ app/src/main/java/com/towhid/tasknote/ac_main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.towhid.tasknote
+package com.towhid.tasknote.ac_main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.towhid.tasknote.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
